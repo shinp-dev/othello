3a09b5c03cf60beb6550e0ad2cddec8f44975046
shinp-dev <84839223+shinp-dev@users.noreply.github.com>
2026-09-26 16:48:35 +0900

    Fix rare badge Compose alignment scope

--- app/src/main/kotlin/com/example/othello/ChanrivaNameSelectionScreen.kt
+++ app/src/main/kotlin/com/example/othello/ChanrivaNameSelectionScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -318,7 +319,7 @@ private fun ChanrivaNameCandidateCard(
 }
 
 @Composable
-private fun RareBadge() {
+private fun BoxScope.RareBadge() {
     Box(
         modifier = Modifier
             .align(Alignment.TopStart)
