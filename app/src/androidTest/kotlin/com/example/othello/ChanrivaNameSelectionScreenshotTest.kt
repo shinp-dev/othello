d5c21e342265d3b1879eef2667f8b7ea5b87a493
shinp-dev <84839223+shinp-dev@users.noreply.github.com>
2026-09-26 17:02:26 +0900

    Use JPEG for Chanriva name screen background

--- app/src/androidTest/kotlin/com/example/othello/ChanrivaNameSelectionScreenshotTest.kt
+++ app/src/androidTest/kotlin/com/example/othello/ChanrivaNameSelectionScreenshotTest.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotEnabled
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.platform.app.InstrumentationRegistry
